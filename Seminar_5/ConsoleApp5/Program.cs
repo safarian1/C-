c73991a8a4d583779dfba8b393ed749a8ec61016
Verónica Safarian <safarianv1@icloud.com>
2023-07-09 17:45:45 +0300

    feat:Seminar6

--- Seminar_5/ConsoleApp5/Program.cs
+++ Seminar_5/ConsoleApp5/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double [] array = new double[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round((new Random().NextDouble() * new Random().Next(0,10)), 2);
+    Console.Write(array[i] + "  ");
+}
+
+double max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+
+Console.WriteLine($"Максимальное значение: {max}");
+
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Минимальное значение: {min}");
+
--- Seminar_6/ConsoleApp6/Program.cs
+++ Seminar_6/ConsoleApp6/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через пробел:");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше нуля: {count}");
+
+
+
+//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите b1:");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1:");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+double k2 = double.Parse(Console.ReadLine());
+
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения двух прямых: {x} {y}");
+
+
